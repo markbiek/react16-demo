55532fc117496207c4adde76e1a84400b944f56e
Mark Biek <support@janustech.net>
2018-07-22 11:57:35 -0400

    Initial add

---
+++ assets/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const $ = jQuery;
+
+class App extends React.Component {
+    render() {
+        return (
+            <p>Hello</p>
+        );
+    }
+}
+
+$(document).ready(() => {
+    let app = document.getElementById('app');
+
+    if (app) {
+        render(<App />, app);
+    }
+});
